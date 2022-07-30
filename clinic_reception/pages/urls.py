9e3d3344210fb973abd43da89e9c14cf82efcfcb
Mehran <31221916+mehranMoslemi@users.noreply.github.com>
2022-07-30 06:05:31 -0700

    reports are added

--- clinic_reception/pages/urls.py
+++ clinic_reception/pages/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("",views.index, name="index"),
-    # path('report-mnt',views.report_mnt,name='rmnt'),
-    # path('report-expenses',views.report_expanses,name='rexp'),
-    # path('report-patient-count',views.report_patient_count,name='rpc'),
-    # path('report-refferal',views.report_refferal,name='ref'),
+    path("",views.index, name="index"),
+    path('mri-p-count',views.report_patient_count_mri,name ='mri_p_count'),
+    path('ct-p-count',views.report_patient_count_ct,name ='ct_p_count'),
+    path('x-p-count',views.report_patient_count_x_ray,name ='x_p_count'),
+
+    path('mri_income',views.income_mri,name ='mri_income'),
+    path('ct_income',views.income_ct,name ='ct_income'),
+    path('x_income',views.income_x,name ='x_income'),
+
+    path('report-referral_mri',views.report_refferal_mri,name='mri_ref'),
+    path('report-referral_ct',views.report_refferal_ct,name='ct_ref'),
+    path('report-referral_x',views.report_refferal_x_ray,name='x_ref'),
+
+    path('report-expenses',views.report_expanses,name='rexp'),
 ]
--- clinic_reception/pages/views.py
+++ clinic_reception/pages/views.py
@@ -1,4 +1,5 @@
 
+from calendar import day_abbr, month
 import datetime
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
@@ -21,103 +22,206 @@ def index(request):
         data = []
         for item in items:
             mri_tot = item.services_mri.aggregate(Sum('fee')).get('fee__sum')
+            date = item.date
+            discount = item.discount
+            data.append({'date':date,'mri':mri_tot,'discount':discount})
+        dt = pd.DataFrame(data)
+        g_list = dt.groupby(['date']).agg(sum)
+        passlist=[]
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index,'mri':row[0],'discount':row[1] ,'total': float(row[0])-float(row[1]),'day':index.day})
+        context = {'values':passlist}
+        print(passlist)
+        return render(request , 'pages/index.html',context)
+
+
+
+@login_required(login_url='login')
+def report_expanses(request):
+    if is_super:
+        items = Expenses.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        data = []
+        for item in items:
+            cost_tot= item.quantity * item.cost
+            data.append({'date':item.date,'cost':cost_tot})
+        dt = pd.DataFrame(data)
+        passlist =[]
+        g_list = dt.groupby(['date']).agg(sum)
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index,'exp':row[0],'day':index.day})
+        context = {'values': passlist}
+        print(passlist)
+        return render(request , 'pages/repexpenses.html',context)
+
+@login_required(login_url='login')
+def report_patient_count_mri(request):
+    if is_super:
+        items = Mri_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        data = []
+        for item in items:
+            mri_tot = item.services_mri.aggregate(Count('id')).get('id__count')
+            date = item.date
+            data.append({'date':date,'mri':mri_tot})
+        dt = pd.DataFrame(data)
+        g_list = dt.groupby(['date']).agg(sum)
+        passlist=[]
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index,'service':row[0],'day':index.day})
+        context = {'values':passlist,'label':'MRI'}
+        print(passlist)
+        return render(request , 'pages/p_count_rep.html',context)
+
+@login_required(login_url='login')
+def report_patient_count_ct(request):
+    if is_super:
+        items = CT_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        data = []
+        for item in items:
+            ct_tot = item.services_ctscan.aggregate(Count('id')).get('id__count')
+            date = item.date
+            data.append({'date':date,'ct':ct_tot})
+        dt = pd.DataFrame(data)
+        g_list = dt.groupby(['date']).agg(sum)
+        passlist=[]
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index,'service':row[0],'day':index.day})
+        context = {'values':passlist,'label':'CT_Scan'}
+        print(passlist)
+        return render(request , 'pages/p_count_rep.html',context)
+
+@login_required(login_url='login')
+def report_patient_count_x_ray(request):
+    if is_super:
+        items = X_Ray_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        data = []
+        for item in items:
+            x_tot = item.services_xray.aggregate(Count('id')).get('id__count')
+            date = item.date
+            data.append({'date':date,'x':x_tot})
+        dt = pd.DataFrame(data)
+        g_list = dt.groupby(['date']).agg(sum)
+        passlist=[]
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index,'service':row[0],'day':index.day})
+        context = {'values':passlist,'label':'X_ray'}
+        print(passlist)
+        return render(request , 'pages/p_count_rep.html',context)
+
+@login_required(login_url='login')
+def income_mri(request):
+    if is_super:
+        items = Mri_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        data = []
+        for item in items:
+            mri_tot = item.services_mri.aggregate(Sum('fee')).get('fee__sum')
+            date = item.date
+            discount = item.discount
+            data.append({'date':date,'mri':mri_tot,'discount':discount})
+        dt = pd.DataFrame(data)
+        g_list = dt.groupby(['date']).agg(sum)
+        passlist=[]
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index,'service':row[0],'discount':row[1] ,'total': float(row[0])-float(row[1]),'day':index.day})
+        context = {'values':passlist,'label':'MRI'}
+        print(passlist)
+        return render(request , 'pages/income.html',context)
+
+@login_required(login_url='login')
+def income_ct(request):
+    if is_super:
+        items = CT_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        data = []
+        for item in items:
             ct_tot = item.services_ctscan.aggregate(Sum('fee')).get('fee__sum')
+            date = item.date
+            discount = item.discount
+            data.append({'date':date,'ct':ct_tot,'discount':discount})
+        dt = pd.DataFrame(data)
+        g_list = dt.groupby(['date']).agg(sum)
+        passlist=[]
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index,'service':row[0],'discount':row[1] ,'total': float(row[0])-float(row[1]),'day':index.day})
+        context = {'values':passlist,'label':'CT Scan'}
+        print(passlist)
+        return render(request , 'pages/income.html',context)
+
+@login_required(login_url='login')
+def income_x(request):
+    if is_super:
+        items = X_Ray_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        data = []
+        for item in items:
             x_tot = item.services_xray.aggregate(Sum('fee')).get('fee__sum')
             date = item.date
-            data.append({'date':date,'mri':mri_tot,'ct':ct_tot,'x':x_tot})
+            discount = item.discount
+            data.append({'date':date,'x':x_tot,'discount':discount})
         dt = pd.DataFrame(data)
-        if dt.size>0:
-            g_list = dt.groupby(['date']).agg(sum)
-            passlist=[]
-            for index, row in g_list.iterrows():
-                passlist.append({'dt':index,'mri':row[0],'ct':row[1],'x':row[2],'day':index.day})
-            context = {'values':passlist}
-            print(passlist)
-            return render(request , 'pages/index.html',context)
-        else:
-            return render(request, 'pages/index.html')
+        g_list = dt.groupby(['date']).agg(sum)
+        passlist=[]
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index,'service':row[0],'discount':row[1] ,'total': float(row[0])-float(row[1]),'day':index.day})
+        context = {'values':passlist,'label':'X Ray'}
+        print(passlist)
+        return render(request , 'pages/income.html',context)
 
 
-# @login_required(login_url='login')
-# def patient(request):
-#     form = PatientForm()
-#     context = {'form':form}
-#     return render(request , 'pages/patient.html',context)
 
-# @login_required(login_url='login')
-# def report_mnt(request):
-#     if is_super:
-#         items = Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
-#         data = []
-#         for item in items:
-#             mri_tot = item.services_mri.aggregate(Sum('fee')).get('fee__sum')
-#             ct_tot = item.services_ctscan.aggregate(Sum('fee')).get('fee__sum')
-#             x_tot = item.services_xray.aggregate(Sum('fee')).get('fee__sum')
-#             date = item.date
-#             data.append({'date':date,'mri':mri_tot,'ct':ct_tot,'x':x_tot})
-#         dt = pd.DataFrame(data)
-#         g_list = dt.groupby(['date']).agg(sum)
-#         passlist=[]
-#         for index, row in g_list.iterrows():
-#             passlist.append({'dt':index,'mri':row[0],'ct':row[1],'x':row[2],'day':index.day})
-#         context = {'values':passlist}
-#         print(passlist)
-#         return render(request , 'pages/reportmnt.html',context)
-
-
-# @login_required(login_url='login')
-# def report_expanses(request):
-#     if is_super:
-#         items = Expenses.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
-#         data = []
-#         for item in items:
-#             cost_tot= item.quantity * item.cost
-#             data.append({'date':item.date,'cost':cost_tot})
-#         dt = pd.DataFrame(data)
-#         passlist =[]
-#         g_list = dt.groupby(['date']).agg(sum)
-#         for index, row in g_list.iterrows():
-#             passlist.append({'dt':index,'exp':row[0],'day':index.day})
-#         context = {'values': passlist}
-#         print(passlist)
-#         return render(request , 'pages/repexpenses.html',context)
-
-# @login_required(login_url='login')
-# def report_patient_count(request):
-#     if is_super:
-#         items = Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
-#         data = []
-#         for item in items:
-#             mri_tot = item.services_mri.aggregate(Count('id')).get('id__count')
-#             ct_tot = item.services_ctscan.aggregate(Count('id')).get('id__count')
-#             x_tot = item.services_xray.aggregate(Count('id')).get('id__count')
-#             date = item.date
-#             data.append({'date':date,'mri':mri_tot,'ct':ct_tot,'x':x_tot})
-#         dt = pd.DataFrame(data)
-#         g_list = dt.groupby(['date']).agg(sum)
-#         passlist=[]
-#         for index, row in g_list.iterrows():
-#             passlist.append({'dt':index,'mri':row[0],'ct':row[1],'x':row[2],'day':index.day})
-#         context = {'values':passlist}
-#         print(passlist)
-#         return render(request , 'pages/reportmnt.html',context)
-
-# @login_required(login_url='login')
-# def report_refferal(request):
-#     if is_super:
-#         items = Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
-#         reflist = []
-#         for item in items:
-#             if item.RefferalPhysician is not None:
-#                 reflist.append({'date':item.date , "name":item.RefferalPhysician.first_name+" "+item.RefferalPhysician.last_name , 'fee':item.RefferalPhysician.fee})
+@login_required(login_url='login')
+def report_refferal_mri(request):
+    if is_super:
+        items = Mri_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        reflist = []
+        for item in items:
+            if item.RefferalPhysician is not None:
+                reflist.append({'date':item.date , "name":item.RefferalPhysician.first_name+" "+item.RefferalPhysician.last_name , 'fee':item.RefferalPhysician.fee})
         
-#         df = pd.DataFrame(reflist)
-#         g_list = df.groupby(['date','name']).agg(sum)
-#         passlist=[]
+        df = pd.DataFrame(reflist)
+        g_list = df.groupby(['date','name']).agg(sum)
+        passlist=[]
         
-#         for index, row in g_list.iterrows():
-#             passlist.append({'dt':index[0],'name':index[1],'fee':row[0],'day':index[0].day})
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index[0],'name':index[1],'fee':row[0],'day':index[0].day})
 
-#         context = {'values':passlist}
-#         print(passlist)
-#         return render(request , 'pages/refferal.html',context)
+        context = {'values':passlist}
+        print(passlist)
+        return render(request , 'pages/refferal.html',context)
+
+@login_required(login_url='login')
+def report_refferal_ct(request):
+    if is_super:
+        items = CT_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        reflist = []
+        for item in items:
+            if item.RefferalPhysician is not None:
+                reflist.append({'date':item.date , "name":item.RefferalPhysician.first_name+" "+item.RefferalPhysician.last_name , 'fee':item.RefferalPhysician.fee})
+        
+        df = pd.DataFrame(reflist)
+        g_list = df.groupby(['date','name']).agg(sum)
+        passlist=[]
+        
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index[0],'name':index[1],'fee':row[0],'day':index[0].day})
+
+        context = {'values':passlist}
+        print(passlist)
+        return render(request , 'pages/refferal.html',context)
+
+@login_required(login_url='login')
+def report_refferal_x_ray(request):
+    if is_super:
+        items = X_Ray_Invoice.objects.filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        reflist = []
+        for item in items:
+            if item.RefferalPhysician is not None:
+                reflist.append({'date':item.date , "name":item.RefferalPhysician.first_name+" "+item.RefferalPhysician.last_name , 'fee':item.RefferalPhysician.fee})
+        
+        df = pd.DataFrame(reflist)
+        g_list = df.groupby(['date','name']).agg(sum)
+        passlist=[]
+        
+        for index, row in g_list.iterrows():
+            passlist.append({'dt':index[0],'name':index[1],'fee':row[0],'day':index[0].day})
+
+        context = {'values':passlist}
+        print(passlist)
+        return render(request , 'pages/refferal.html',context)
